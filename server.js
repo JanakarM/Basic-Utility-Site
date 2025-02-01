5ed2cb64bbf76b48d8d1a6e74efb1f3152f4f6f9
Janakar Muthuraj <janakar.m@servicenow.com>
2025-02-01 21:59:28 +0530

    Static files served

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send('Hello World!\nWelcome to My Node.js app simple');
+    res.sendFile(__dirname + '/index.html');
 });
 app.listen(3000, () => {
     console.log('Server started on port 3000');
