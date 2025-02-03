287a02622cac4218cd410adac31af02cc14ee064
Janakar Muthuraj <janakar.muthuraj@gmail.com>
2025-02-03 09:29:24 +0530

    basic layout style changes

--- server.js
+++ server.js
@@ -3,7 +3,10 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/public/index.html');
+});
+app.get('/style.css', (req, res) => {
+    res.sendFile(__dirname + '/public/style.css');
 });
 app.listen(3000, () => {
     console.log('Server started on port 3000');
