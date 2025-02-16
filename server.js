f38c5616636679f4c5b570bb512eed1ff927067b
Janakar Muthuraj <janakar.muthuraj@gmail.com>
2025-02-16 22:36:51 +0530

    checking environment variables revert

--- server.js
+++ server.js
@@ -3,8 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-    // res.sendFile(__dirname + '/public/index.html');
-    res.send("Name: " + process.env.NAME);
+    res.sendFile(__dirname + '/public/index.html');
 });
 app.get('/public', (req, res) => {
     res.sendFile(__dirname + '/public/' + req.query.id);
